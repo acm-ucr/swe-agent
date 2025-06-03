d4fb557a52de0c793454e2e3bb3ebde710d98f49
Jerry Li <56666748+Jeli04@users.noreply.github.com>
2025-06-02 22:58:20 -0700

    parse args

--- run_agent.py
+++ run_agent.py
@@ -3,6 +3,7 @@ import json
 import os
 import zmq 
 import threading
+import argparse
 from colorama import init, Fore, Style
 from agents.interface_agent.interface_agent import InterfaceAgent 
 from agents.review_agent.review_agent import ReviewAgent
@@ -13,8 +14,16 @@ from shared.file_tools import extract_json
 from orchestrator.orchestrator import Orchestrator
 from orchestrator.client import start_handshake_server, text_listener   
 
+def parse_args():
+    parser = argparse.ArugementParser(description="SWE Agent Settings")
+    parser.add_argument("--instruction_path", type=str, default="./instructions/")
+    parser.add_argument("--device_config", type=str, default="")
+    parser.add_argument("--distributed_config", type=str, default="")
+    parser.add_argument("--log_type", type=str, default="test")
+    parser.add_argument("--main_device", type=bool, description="On distributed setting the task divider, no distributed the main agent", default=True)
 
-def run_code_agent():
+
+def run_code_agent(args):
     # Render ASCII art banner
     banner = pyfiglet.figlet_format("SWE Agent", font="slant")
 
@@ -22,11 +31,15 @@ def run_code_agent():
     print(Fore.LIGHTBLUE_EX + banner + Style.RESET_ALL)
 
     # intialize variables 
-    path = "/Users/jerryli/Desktop/python/SWE-Agent/instructions/code_only/test3.json" # change later
+    path = args.instruction_path # "/Users/jerryli/Desktop/python/SWE-Agent/instructions/code_only/test3.json" 
     instructions = read_initial_instructions(path)
-    log_path = setup_logs(type='tests')
-    with open("instructions/code_only/device.json") as f:
-        device_config = json.load(f)
+    log_path = setup_logs(type=args.log_type)
+    if args.device_config != "":
+        with open(args.device_config) as f:   # "instructions/code_only/device.json"
+            device_config = json.load(f)
+        distributed = False
+    else:   
+        distributed = True
 
     # intialize agent
     code_agent = CodingAgent("cogito:3b", 
@@ -65,10 +78,8 @@ def run_code_agent():
 
     # send to aggregator
  
-                        
 
-
-def run_main_agent():
+def run_main_agent(args):
     # Render ASCII art banner
     banner = pyfiglet.figlet_format("SWE Agent", font="slant")
 
@@ -76,44 +87,25 @@ def run_main_agent():
     print(Fore.LIGHTBLUE_EX + banner + Style.RESET_ALL)
 
     # intialize variables 
-    path = r"C:\Users\10660\Projects\swe-agent\instructions\test2.json" # change later
+    path = args.instruction_path # "/Users/jerryli/Desktop/python/SWE-Agent/instructions/code_only/test3.json" 
     instructions = read_initial_instructions(path)
-    log_path = setup_logs(type='tests')
-    devices = {
-        "regular": [
-            {
-                "id": 1,
-                "ip": 1234,
-                "port": 5555,
-                "status": "open"  # open, closed
-            },
-            {
-                "id": 2,
-                "ip": 1235,
-                "port": 5555,
-                "status": "open"  # open, closed
-            },
-            {
-                "id": 3,
-                "ip": 1236,
-                "port": 5555,
-                "status": "open"  # open, closed
-            },
-        ],
-        "thinking": [
-            {
-                "id": 4,
-                "ip": 1237,
-                "port": 5555,
-                "status": "open"  # open, closed
-            },
-        ]
-    }
+    log_path = setup_logs(type=args.log_type)
+    if args.device_config != "":
+        with open(args.device_config) as f:   # "instructions/code_only/device.json"
+            device_config = json.load(f)
+        with open(args.distributed_config) as f:
+            distributed_config = json.load(f)
+        distributed = False
+    else:   
+        distributed = True
 
     # intialize agents
     interface_agent = InterfaceAgent("gemma3:12b", "ollama", sys_msg=instructions['interaction_prompt'], temperature=0.5)
-    orchestrator_agent = Orchestrator("cogito:3b", "ollama", sys_msg=instructions['orchestrator_prompt'], devices=devices)
-
+    orchestrator_agent = Orchestrator("cogito:3b", "ollama", sys_msg=instructions['orchestrator_prompt'], devices=distributed_config)
+    code_agent = CodingAgent("cogito:3b", 
+                             "ollama", 
+                             sys_prompt=instructions['code_prompt']['system_prompt'], 
+                             correction_prompt=instructions['code_prompt']['correction_prompt']) if not distributed else None
     # interaction 
     while True:
         user_prompt = input(">>> ")
@@ -135,22 +127,33 @@ def run_main_agent():
         if tasks is not None:
             break
 
-
     # orchestrator 
-    print(tasks)
-    log_interaction(log_path, 
-                    {
-                        "agent": "interaction",
-                        "tasks": tasks,
-                    })
-    orchestrator_agent.stream_tasks(tasks, log_path=log_path)
+    if distributed:
+        print(tasks)
+        log_interaction(log_path, 
+                        {
+                            "agent": "interaction",
+                            "tasks": tasks,
+                        })
+        orchestrator_agent.stream_tasks(tasks, log_path=log_path)
 
     # code agent
+    if not distributed:
+        file_tree = ""
+        for i, task in enumerate(tasks):
+            output = code_agent.analyze_task(file_tree, task, max_tries=10)   # files to modify
+            # result = code_agent.check_status(dummy_script_path, id)
 
+def main():
+    args = parse_args()
+
+    if not args.main_device:
+        run_code_agent(args)
+    else:
+        run_main_agent(args)
 
 if __name__ == "__main__":
-    run_code_agent()
-    # run_main_agent()
+    main()
 
 
 # test
